100816606a80ad8e1c7edf39f76abfc29c63f8b2
Guy Heath <guy.heath@me.com>
2021-07-26 16:19:14 +0100

    Populated train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -7,23 +7,15 @@ from sqlalchemy import create_engine
 import pickle
 
 # Import from scikit-learn
-# TODO: Tidy up anything unused and combined, order where possible
-from sklearn.metrics import confusion_matrix
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.multioutput import MultiOutputClassifier
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.pipeline import Pipeline, FeatureUnion
-from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
 
-# Import nltk and download required files
+# Import nltk
 import nltk
-from nltk.corpus import stopwords
-from nltk.stem.wordnet import WordNetLemmatizer
-from nltk.tokenize import word_tokenize, sent_tokenize
-
-nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
 
 def load_data(database_filepath):
     """ Load data from database file
@@ -51,36 +43,118 @@ def load_data(database_filepath):
 
 
 def tokenize(text):
-    pass
+    """ Tokenise, standardise, lemmatize, and strip URLs from text
+
+    Arguments:
+        text (str): text to process
+
+    Returns:
+        clean_tokens (list): list of clean tokens
+    """
+
+    # Replace URLs in text with a standard placeholder
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+
+    # Remove non-alphanumeric characters
+    text = re.sub(r"[^A-Za-z0-9]", " ", text)
+    
+    # Tokenise, lemmatise, and convert to lower case
+    tokens = nltk.word_tokenize(text)
+    lemmatizer = nltk.WordNetLemmatizer()    
+    clean_tokens = [lemmatizer.lemmatize(w).lower().strip() for w in tokens]
+
+    return clean_tokens
 
 
-def build_model():
-    pass
+def build_model(gridsearch = False):
+    """ Build model pipeline and optionally create optimisation matrix with GridSearchCV
+    
+    Arguments:
+        gridsearch (bool): apply GridSearchCV to optimise model parameters (default False)
 
+    Returns:
+        pipeline (Pipeline): model
+    """
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(AdaBoostClassifier()))
+    ])
 
-def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    # If selected, use GridSearchCV to optimise parameters
+    if gridsearch == True:
+        print("Model parameters will be optimised (may take some time).")
+        parameters = {
+            'vect__ngram_range': ((1, 1), (1, 2)),
+            'vect__stop_words' : (None, 'english'),
+            'clf__estimator__min_samples_split': (2, 3)
+        }
+
+        pipeline = GridSearchCV(pipeline, param_grid=parameters, verbose=10)
+
+    return pipeline
+
+def evaluate_model(model, X_test, y_test, category_names):
+    """ Test model performance using test data and print results
+
+    Arguments:
+        model (Pipeline): model to evaluate
+        X_test (DataFrame): messages test data
+        y_test (DataFrame): categories test data
+        category_names (list): list of category names
+    """
+    # Predict on test data
+    y_pred = model.predict(X_test)
+
+    # Print classification report for each category
+    for i, column_name in enumerate(category_names):
+        print("Response: {}".format(column_name))
+        print(classification_report(y_test.iloc[:,i], y_pred[:,i]))
+        print("=====")
 
 
 def save_model(model, model_filepath):
-    pass
+    """ Save model to file for future use
+
+    Arguments:
+        model (object): model to save to file
+        model_filepath (string): location to save model
+    """
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
-    if len(sys.argv) == 3:
-        database_filepath, model_filepath = sys.argv[1:]
+    # Valid scenarios are 2 or 3 arguments. Otherwise display usage guide 
+    if (len(sys.argv) == 3) | (len(sys.argv) == 4):
+        # Database and model filepaths are the first two args after the script name
+        database_filepath, model_filepath = sys.argv[1:3]
+        # Check if there's a fourth argument to enable GridSearchCV
+        use_gridsearch = False
+        try:
+            if sys.argv[3] == '--use_gridsearch':
+                use_gridsearch = True
+        except IndexError:
+            pass
+
+        print("Downloading required files from the Natural Language Toolkit...")
+        nltk.download(['punkt', 'wordnet'])
+
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
-        X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        X, y, category_names = load_data(database_filepath)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
         
         print('Building model...')
-        model = build_model()
+        model = build_model(gridsearch=use_gridsearch)
         
         print('Training model...')
-        model.fit(X_train, Y_train)
+        model.fit(X_train, y_train)
         
         print('Evaluating model...')
-        evaluate_model(model, X_test, Y_test, category_names)
+        evaluate_model(model, X_test, y_test, category_names)
 
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
         save_model(model, model_filepath)
@@ -91,7 +165,9 @@ def main():
         print('Please provide the filepath of the disaster messages database '\
               'as the first argument and the filepath of the pickle file to '\
               'save the model to as the second argument. \n\nExample: python '\
-              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl'\
+              '\n\nOptionally, append the \'--use_gridsearch\' parameter to use '\
+              'GridSearch CV to optimise model parameters.')
 
 
 if __name__ == '__main__':
