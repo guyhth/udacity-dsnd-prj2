8d752c4fa62565692b09134364b377e642f6a5d5
Guy Heath <guy.heath@me.com>
2021-07-27 13:18:51 +0100

    Added two plotly visualisations of training data

--- app/run.py
+++ app/run.py
@@ -1,36 +1,54 @@
 import json
+from os import name
 import plotly
 import pandas as pd
+import re
 
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
+import nltk
+nltk.download(['punkt', 'wordnet'])
 
 from flask import Flask
 from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+from plotly.graph_objs import Bar, Histogram
+import joblib
 from sqlalchemy import create_engine
 
 
 app = Flask(__name__)
 
 def tokenize(text):
-    tokens = word_tokenize(text)
-    lemmatizer = WordNetLemmatizer()
+    """ Tokenise, standardise, lemmatize, and strip URLs from text
 
-    clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_tok)
+    Arguments:
+        text (str): text to process
+
+    Returns:
+        clean_tokens (list): list of clean tokens
+    """
+
+    # Replace URLs in text with a standard placeholder
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+
+    # Remove non-alphanumeric characters
+    text = re.sub(r"[^A-Za-z0-9]", " ", text)
+    
+    # Tokenise, lemmatise, and convert to lower case
+    tokens = nltk.word_tokenize(text)
+    lemmatizer = nltk.WordNetLemmatizer()    
+    clean_tokens = [lemmatizer.lemmatize(w).lower().strip() for w in tokens]
 
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('MessagesAndCategories', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
@@ -38,33 +56,65 @@ model = joblib.load("../models/your_model_name.pkl")
 @app.route('/index')
 def index():
     
-    # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
-    genre_counts = df.groupby('genre').count()['message']
-    genre_names = list(genre_counts.index)
-    
-    # create visuals
-    # TODO: Below is an example - modify to create your own visuals
+    # Create visuals
+    # Define list of graphs and their layout properties
     graphs = [
         {
-            'data': [
-                Bar(
-                    x=genre_names,
-                    y=genre_counts
-                )
-            ],
+            'data': [], # Data populated in code below
 
             'layout': {
-                'title': 'Distribution of Message Genres',
+                'title': 'Message Genres by Category',
                 'yaxis': {
-                    'title': "Count"
+                    'dtick': 1
                 },
                 'xaxis': {
-                    'title': "Genre"
+                    'title': "Count"
+                },
+                'barmode': "stack",
+                'height': 1000,
+                'margin': {
+                    'l': 200
+                }
+            }
+        },
+        {
+            'data': [], # Data populated in code below
+
+            'layout': {
+                'title': 'Distribution of Message Lengths',
+                'yaxis': {
+                    'title': "Frequency"
+                },
+                'xaxis': {
+                    'title': "Characters in message",
+                    'range': [0, 500]
                 }
             }
         }
     ]
+
+    # Extract data for first graph and create Graph Objects
+    # Count of each category, split by genre
+    genre_counts = df.groupby('genre').count()['message']
+    genre_names = list(genre_counts.index)
+    category_names = df.iloc[:, 4:].columns.str.replace('_', ' ')
+    category_names = [category.title() for category in category_names]
+
+    for genre in genre_names:
+        category_counts = df[df['genre'] == genre].iloc[:, 4:].sum()
+        graphs[0]['data'].append(Bar(
+            name=genre.title(),
+            y=category_names,
+            x=category_counts,
+            orientation= 'h'
+        ))
+
+    # Extract data for second graph and create Graph Objects
+    # Histogram of message lengths
+    message_lengths = df['message'].str.len()
+    graphs[1]['data'].append(Histogram(
+        x=message_lengths
+    ))
     
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
